b59129cf30ca2dac2e253c3d3fdcb153d810c915
Tobi <tbravmann@bavariantools.org>
2020-02-27 20:00:05 +0100

    Added flexbox for buttons

--- src/webparts/spfx2019/components/ButtonRowComp.tsx
+++ src/webparts/spfx2019/components/ButtonRowComp.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import styles from '../../../../lib/webparts/spfx2019/components/Spfx2019.module.scss';
 
 
 export interface IButtonRowCompProps {
@@ -13,12 +14,14 @@ export class ButtonRowComp extends React.Component<IButtonRowCompProps,{}>{
     render(){
         return(
 
-            <div>
-
+            <div className = {styles.buttonbox}>
+                   
                 <PrimaryButton text="Apply" onClick={this._alertClicked} disabled={this.props.disabled} checked={this.props.disabled} />      
-                
+               
                 
                 <PrimaryButton text="Discard" onClick={this._alertClicked} disabled={this.props.disabled} checked={this.props.disabled} />
+               
+                
                 
             </div>
 
